a05204e77fac2a315b55318bfd438aa55f5fd92d
Muhle Mabunda <134883431+Shakira2022@users.noreply.github.com>
2026-01-08 04:24:23 +0200

    Update route.ts

--- app/api/forgot-password/route.ts
+++ app/api/forgot-password/route.ts
@@ -28,7 +28,6 @@ function generateRandomPassword(length = 8) {
 }
 
 /**
- * Convert local SA number (e.g. 0698145345) → +27698145345
  */
 function formatPhoneNumber(contact: string): string {
     // Remove all non-digits
